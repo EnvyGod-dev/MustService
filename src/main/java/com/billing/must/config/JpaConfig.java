4a06065c40dc32e0c443eb2dd12abfded8ababba
Bilguun Munkhbat <bilguun@callpro.local>
2025-04-02 11:53:59 +0800

    check

--- src/main/java/com/billing/must/config/JpaConfig.java
+++ src/main/java/com/billing/must/config/JpaConfig.java
@@ -1,10 +1,11 @@
 package com.billing.must.config;
 
 import javax.sql.DataSource;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -15,33 +16,37 @@ import org.springframework.transaction.PlatformTransactionManager;
 @EnableJpaRepositories(basePackages = "com.billing.must.repository")
 public class JpaConfig {
 
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
+
+    @Value("${spring.datasource.username}")
+    private String datasourceUsername;
+
+    @Value("${spring.datasource.password}")
+    private String datasourcePassword;
+
     @Bean
+    @Primary
     public DataSource dataSource() {
         return DataSourceBuilder.create()
                 .driverClassName("org.postgresql.Driver")
-                .url("jdbc:postgresql://103.50.205.42:5432/Must")
-                .username("Worms")
-                .password("Worms1231@")
+                .url(datasourceUrl)
+                .username(datasourceUsername)
+                .password(datasourcePassword)
                 .build();
     }
 
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
         HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        vendorAdapter.setShowSql(true);       // Show SQL in the logs
-        vendorAdapter.setGenerateDdl(true);     // Let Hibernate generate/update the schema
+        vendorAdapter.setShowSql(true);
+        vendorAdapter.setGenerateDdl(true);
         vendorAdapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
 
         LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
         factoryBean.setDataSource(dataSource);
         factoryBean.setJpaVendorAdapter(vendorAdapter);
-        // Set the package where your JPA entity classes are located
         factoryBean.setPackagesToScan("com.billing.must.model");
-
-        // Optionally, you can add additional JPA properties:
-        // Map<String, Object> jpaProperties = new HashMap<>();
-        // jpaProperties.put("hibernate.hbm2ddl.auto", "update");
-        // factoryBean.setJpaPropertyMap(jpaProperties);
 
         return factoryBean;
     }
